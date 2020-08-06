5a16c50a149af1607f7054341411b85d6d1761ec
Nikita Kochetov <nothsmple@mail.ru>
2020-08-06 23:56:13 +0300

    Final

--- TConverter_/ViewController.swift
+++ TConverter_/ViewController.swift
@@ -12,9 +12,19 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var celsiusLabel: UILabel!
     @IBOutlet weak var fahrenheitLabel: UILabel!
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+    }
     
     @IBAction func sliderChanged(_ sender: UISlider) {
+        let temperatureCelsius = Int(round(sender.value))
+        celsiusLabel.text = "\(temperatureCelsius)ºC"
+        let farenheitTemperature = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLabel.text = "\(farenheitTemperature)ºF"
     }
     
 
